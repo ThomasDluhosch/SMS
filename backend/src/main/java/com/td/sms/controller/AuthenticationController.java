2a9ada8491eac4b09e396df6dae49ed90e4dc87c
Thomas <t.dluhosch@gmail.com>
2025-09-24 00:10:54 +0200

    add User Authentication

---
+++ backend/src/main/java/com/td/sms/controller/AuthenticationController.java
@@ -0,0 +1,36 @@
+package com.td.sms.controller;
+
+import com.td.sms.model.AuthenticationResponse;
+import com.td.sms.model.User;
+import com.td.sms.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    private final AuthenticationService authService;
+
+
+    public AuthenticationController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody User request
+    ) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody User request
+    ) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
