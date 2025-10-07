a9d0e3212f32c3772eee459c5bfc84a49a33d3a1
Thomas <t.dluhosch@gmail.com>
2025-10-07 13:09:20 +0200

    add MUI

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,5 +1,7 @@
 import "./SideBar.css";
 import { NavLink } from "react-router-dom";
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import GroupsIcon from '@mui/icons-material/Groups';
 
 const SideBar = () => {
 	const userRole = localStorage.getItem("userRole");
@@ -19,26 +21,7 @@ const SideBar = () => {
 
 				<div className="sideBarLinks">
 					<NavLink className="link" to="/dashboard">
-						<svg
-							width="20px"
-							height="20px"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M12 12C12 11.4477 12.4477 11 13 11H19C19.5523 11 20 11.4477 20 12V19C20 19.5523 19.5523 20 19 20H13C12.4477 20 12 19.5523 12 19V12Z"
-								stroke-linecap="round"
-							/>
-							<path
-								d="M4 5C4 4.44772 4.44772 4 5 4H8C8.55228 4 9 4.44772 9 5V19C9 19.5523 8.55228 20 8 20H5C4.44772 20 4 19.5523 4 19V5Z"
-								stroke-linecap="round"
-							/>
-							<path
-								d="M12 5C12 4.44772 12.4477 4 13 4H19C19.5523 4 20 4.44772 20 5V7C20 7.55228 19.5523 8 19 8H13C12.4477 8 12 7.55228 12 7V5Z"
-								stroke-linecap="round"
-							/>
-						</svg>
+						<SpaceDashboardIcon />
 						Dashboard
 					</NavLink>
 
@@ -53,15 +36,7 @@ const SideBar = () => {
 								}}
 							/>
 							<NavLink className="link" to="/users">
-								<svg
-									width="20px"
-									height="20px"
-									viewBox="0 0 24 24"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" />
-								</svg>
+								<GroupsIcon />
 								Alle Mitarbeiter
 							</NavLink>
 						</>
@@ -70,7 +45,9 @@ const SideBar = () => {
 			</div>
 			<div>
 				{userName}
-				<button className="logOutBtn" onClick={handleLogout}>Abmelden</button>
+				<button className="logOutBtn" onClick={handleLogout}>
+					Abmelden
+				</button>
 			</div>
 		</div>
 	);
